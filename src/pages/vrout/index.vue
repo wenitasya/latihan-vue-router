3a87076dfce580350fd920fd2bf38622bbc56a79
wenitasya <95828902+wenitasya@users.noreply.github.com>
2022-03-28 17:14:16 +0700

    Update index.vue

--- src/pages/vrout/index.vue
+++ src/pages/vrout/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <img src="../assets/vrt.png" /> 
+
     <h5> Vue router adalah router resmi miliki vue js, dimana penggunaanya sangat mudah untuk pembuatan single page application. Note: Single Page Application adalah istilah untuk membuat web dinamis yang tidak perlu merequest seluruh halaman ke server.</h5>
 </template>
 
